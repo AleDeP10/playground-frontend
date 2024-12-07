98e1b271a59c31a866c179c5c9b266a4f25c1cd8
AleDeP10 <alessandro.deprato@outlook.com>
2024-12-07 22:00:26 +0100

    AddEditModal operativa

--- src/components/AddEditModal.js
+++ src/components/AddEditModal.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { crea, aggiorna } from "../api/todo_list";
 import "./Modal.css";
 
-const AddEditModal = ({ isOpen, onClose, task }) => {
+const AddEditModal = ({ isOpen, onClose, taskItem }) => {
+  const [task, setTask] = useState(taskItem?.task || "");
+  const [status, setStatus] = useState(taskItem?.status || "TODO");
+
+  const save = () => {
+    if (taskItem) {
+      aggiorna(taskItem.id, task, status);
+    } else {
+      crea(task, status);
+    }
+    setTask('');
+    setStatus('TODO');
+  }
+
+  const saveAndClose = () => {
+    save();
+    onClose();
+  }
+
   if (!isOpen) return null;
 
   return (
@@ -10,9 +29,29 @@ const AddEditModal = ({ isOpen, onClose, task }) => {
         <span className="close" onClick={onClose}>
           &times;
         </span>
-        <h2>
-          {task ? 'Edit Task' : 'New Task'}
-        </h2>
+        <h2>{taskItem ? "Edit Task" : "New Task"}</h2>
+        <div>
+          <div className="input-container">
+            <span className="task-label">Task</span>
+            <input
+              type="text"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+          <span className="task-label">Status</span>
+            <select value={status} onChange={(e) => setStatus(e.target.value)}>
+              <option value="TODO">TODO</option>
+              <option value="IN PROGRESS">IN PROGRESS</option>
+              <option value="DONE">DONE</option>
+            </select>
+          </div>
+        </div>
+        <div className="buttons">
+          <button onClick={saveAndClose}>Save</button>
+          <button onClick={save}>Save and new</button>
+        </div>
       </div>
     </div>
   );
--- src/components/CreditsModal.js
+++ src/components/CreditsModal.js
@@ -1,4 +1,6 @@
 import React from "react";
+import AddImg from "../assets/add_9055025.png";
+import ArrowImg from "../assets/right-arrow_5690084.png";
 import "./Modal.css";
 
 const CreditsModal = ({ isOpen, onClose }) => {
@@ -13,7 +15,12 @@ const CreditsModal = ({ isOpen, onClose }) => {
         <h2>Credits</h2>
         <ul>
           <li>
+            <img src={AddImg} alt="new" />
             <a href="https://www.freepik.com/search">Icon by Fathema Khanom</a>
+          </li>
+          <li>
+            <img src={ArrowImg} alt="next" />
+            <a href="https://www.freepik.com/search">Icon by IYAHICON</a>
           </li>
         </ul>
       </div>
